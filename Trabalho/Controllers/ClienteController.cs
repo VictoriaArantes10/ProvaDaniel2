f4e28790361a098af9a71f285cbde3101de1cec7
Victoria <victoria_amrosa@hotmail.com>
2022-04-26 21:03:29 -0300

    metodo delete

---
+++ Trabalho/Controllers/ClienteController.cs
@@ -0,0 +1,48 @@
+﻿using Projeto.Context;
+using Projeto.Models;
+using System.Linq;
+using System.Net;
+using System.Web.Mvc;
+
+namespace Trabalho.Controllers
+{
+    public class ClienteController : Controller
+    {
+        private readonly Contexto _contexto = new Contexto();
+
+        // GET: Cliente
+        public ActionResult Index()
+        {
+            var clientes = _contexto.Clientes.ToList();
+            return View(clientes);
+        }
+
+        [HttpGet]
+        public ActionResult Delete(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            Cliente cliente = _contexto.Clientes.Where(c => c.Id == id).FirstOrDefault();
+
+            if (cliente == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(cliente);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(long id)
+        {
+            Cliente cliente = _contexto.Clientes.Find(id);
+            _contexto.Clientes.Remove(cliente);
+            _contexto.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
